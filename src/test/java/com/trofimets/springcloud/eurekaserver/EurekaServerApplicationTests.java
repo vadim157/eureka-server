4fc7c3983520b3960a8faa214cd5e790e149337b
vadim157 <trofimets@inbox.ru>
2022-10-31 14:00:26 +0300

    Initial commit

---
+++ src/test/java/com/trofimets/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trofimets.springcloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
